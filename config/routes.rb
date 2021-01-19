f4b403414b0c7442037aaf02451c0f9d7c12cba2
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-19 00:39:03 -0700

    Add route for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: :index
       resources :users, only: :create
+      resources :sessions, only: [:create]
     end
   end
 end
