9cd040d4d8c098924e29b171021e579bac28b5da
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-19 00:20:53 -0700

    Test another city to confirm function of endpoint

--- spec/requests/api/v1/forecast/forecast_for_a_city_spec.rb
+++ spec/requests/api/v1/forecast/forecast_for_a_city_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
-describe 'The forcast endpoint' do
+describe 'The forecast endpoint' do
   it 'returns the weather for a city', :vcr do
     get '/api/v1/forecast', params: {location: 'denver,co'}
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    forecast = JSON.parse(response.body, symbolize_names: true)
+
+    expect(forecast[:data][:type]).to eq('forecast')
+    expect(forecast[:data][:attributes]).to have_key(:current_weather)
+    expect(forecast[:data][:attributes]).to have_key(:hourly_weather)
+    expect(forecast[:data][:attributes]).to have_key(:daily_weather)
+  end
+
+  it 'retuns the weather for another city', :vcr do
+    get '/api/v1/forecast', params: {location: 'Florence, AL'}
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
