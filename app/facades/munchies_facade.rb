b28cdc7bd312aa0cf4be965ecbb2d18986bf1772
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-19 10:48:46 -0700

    Munchies facade update

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,8 +1,15 @@
 class MunchiesFacade
 
-  def get_destination_city(start, destination)
+  def get_destination_city(start_here, destination)
     coords = LatLonFacade.get_coordinates(destination)
+    travel_time = self.get_travel_time(start_here, destination)
     dest = DestinationCity.new(coords)
-    travel_time = EstTravelFacade.get_travel_time(start, destination)
+  end
+
+  def get_travel_time(start_here, destination)
+    tt = EstTravelFacade.get_travel_time(start_here, destination)
+    trav_time = TravTime.new(tt)
+    require "pry"
+    binding.pry
   end
 end
