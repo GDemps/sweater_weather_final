0eecd6dd712d36c6201b290475096107f04846e4
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-20 11:17:46 -0700

    Foodie controller complete

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -4,8 +4,7 @@ class Api::V1::FoodieController < ApplicationController
     location = LatLonFacade.get_coordinates(params[:end])
     forecast = ForecastFacade.get_forecast(location)
     restaurant = RestaurantFacade.get_restaurant(location, params[:search])
-    require "pry"
-    binding.pry
-    render json: FoodieSerializer.new(directions, location, forecast)
+    foodie = Foodie.new(directions, location, forecast, restaurant)
+    render json: FoodieSerializer.new(foodie)
   end
 end
