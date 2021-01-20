e57971381b2e266877c730be8e1ed677f24fed1f
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-20 10:22:40 -0700

    Create restaurant service

---
+++ app/services/restaurant_service.rb
@@ -0,0 +1,21 @@
+class RestaurantService
+  def get_restaurant(latlon, search)
+    response = conn.get('/api/v2.1/search') do |req|
+      req.params['search'] = search
+      req.params['lat'] = latlon.lat
+      req.params['lon'] = latlon.lon
+    end
+    parse_data(response)
+  end
+
+  def parse_data(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+  def conn
+    Faraday.new('https://developers.zomato.com') do |req|
+      req.headers['user-key'] = ENV['ZOMATO_API_KEY']
+    end
+  end
+end
