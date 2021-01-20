61222db47208a0dd35dec6f5b2cb5a5827e0ed2c
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-20 10:23:14 -0700

    Retake spec to test endpoint

--- spec/requests/api/v1/retake/retake_req_spec.rb
+++ spec/requests/api/v1/retake/retake_req_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe 'The retake endpoint' do
   it 'returns end city, estimated travel time, name/address of restaurant, and arrival forecast', :vcr do
-    get '/api/v1/foodie', , params: {start: 'denver,co', end: 'pueblo,co'}
+    get '/api/v1/foodie', params: {start: 'denver,co', end: 'pueblo,co', search: 'burgers'}
     expect(response).to be_successful
     expect(response.status).to eq(200)
 
     retake_data = JSON.parse(response.body, symbolize_names: true)
-
+    require "pry"
+    binding.pry
     # expect(retake_data[:data][:attributes]).to have_key(:current_weather)
     # expect(retake_data[:data][:attributes]).to have_key(:hourly_weather)
     # expect(retake_data[:data][:attributes]).to have_key(:daily_weather)
